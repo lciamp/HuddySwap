91d23283b3eee4017e152cd836f7bba5239a6533
lciamp <l.ciamp@yahoo.com>
2023-03-11 12:33:38 -0500

    add wallet functions

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import { ethers } from 'ethers';
 
 function App() {
 
-  /// for interacting with the blockchain
+  // for interacting with the blockchain
   const [provider, setProvider] = useState(undefined)
 
-  /// set the signer so you can write to the blockchain
+  // set the signer so you can write to the blockchain
   const [signer, setSigner] = useState(undefined)
   
-  /// to check what is in the signers wallet
+  // to check what is in the signers wallet
   const [signerAddress, setSigner] = useState(undefined)
 
-  /// function for setting the provider
+  // function for setting the provider
   useEffect(() => {
     const onLoad = async () => {
       const provider = await new ethers.providers.Web3Provider(window.ethereum)
@@ -22,8 +22,29 @@ function App() {
     onLoad()
   }, [])
 
+  // functions for connecting to the wallet
+  const getSigner = async provider => {
+    provider.send("eth_RequestAccounts", [])
+    const signer = provider.getSigner()
+    setSigner(signer)
+  }
 
+  // check it wallet is connected
+  const isConnected = () => signer !== undefined 
 
+  // get the wallet address
+  const getWalletAddress = () => {
+    signer.getAddress()
+      .then(address => {
+        setSignerAddress(address)
+
+        // todo: connect weth with uni contracts
+      } )
+  }
+
+  if (signer !== undefined){
+    getWalletAddress()
+  }
 
   return (
     <div className="App">

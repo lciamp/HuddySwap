08f4761606bcb96ad7f90ee9595fadb0651d83b6
lciamp <l.ciamp@yahoo.com>
2023-03-11 12:21:53 -0500

    add basic vars and provider function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import { useState, useEffect} from 'react';
 import { ethers } from 'ethers';
 
 function App() {
+
+  /// for interacting with the blockchain
+  const [provider, setProvider] = useState(undefined)
+
+  /// set the signer so you can write to the blockchain
+  const [signer, setSigner] = useState(undefined)
+  
+  /// to check what is in the signers wallet
+  const [signerAddress, setSigner] = useState(undefined)
+
+  /// function for setting the provider
+  useEffect(() => {
+    const onLoad = async () => {
+      const provider = await new ethers.providers.Web3Provider(window.ethereum)
+      setProvider(provider)
+    }
+    onLoad()
+  }, [])
+
+
+
+
   return (
     <div className="App">
 

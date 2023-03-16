a4ce61fcad865ffd2c6360e258bb45475b6bbc6f
lciamp <l.ciamp@yahoo.com>
2023-03-16 14:22:37 -0400

    start implmenting alpha router

--- src/AlphaRouterService.js
+++ src/AlphaRouterService.js
@@ -3,3 +3,16 @@ const { Token, CurrencyAmount, TradeType, Percent } = require('@uniswap/sdk-core
 const { ethers, BigNumber } = require('ethers')
 const JSBI = require('jsbi')
 const ERC20ABI = require('./abi.json')
+
+// swap router wallet address
+const V3_SWAP_ROUTER_ADDRESS = '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
+
+// don't need to explicitly use env lib, just add REACT_APP_
+const REACT_APP_INFURA_URL_TESTNET = process.env.REACT_APP_INFURA_URL_TESTNET
+
+// set chain id for Ethereum's Ropsten Testnet
+// todo: switch to Kiln or Goerli
+const chainId = 3
+
+const web3Provider = new ethers.providers.JsonRpcProvider(REACT_APP_INFURA_URL_TESTNET)
+
